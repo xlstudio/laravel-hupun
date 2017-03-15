789a70418852092912455838b89445c593a98408
Xiphin <Xiphin@qq.com>
2017-03-15 18:12:22 +0800

    Mod:修改config/hupun.php文件

--- config/hupun.php
+++ config/hupun.php
@@ -2,13 +2,13 @@
 
 return [
 
-    'hupun' => [
-        'api_key' => env('HUPUN_API_KEY'),
-        'api_serect' => env('HUPUN_API_SERECT'),
-        'api_options' => [
-            'api_url' => env('HUPUN_API_URL'),
-            'api_work_dir' => storage_path('app/hupun/data'),
-        ],
+    'api_key' => env('HUPUN_API_KEY'),
+
+    'api_serect' => env('HUPUN_API_SERECT'),
+
+    'api_options' => [
+        'api_url' => env('HUPUN_API_URL'),
+        'api_work_dir' => storage_path('app/hupun/data'),
     ],
 
 ];
