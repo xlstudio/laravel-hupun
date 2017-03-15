bf85e924902e2df78a6a4f45f8d24dc6b40abe0c
Xiphin <Xiphin@qq.com>
2017-03-15 18:04:03 +0800

    Fix:修改config/hupun.php的BUG

--- config/hupun.php
+++ config/hupun.php
@@ -1,12 +1,14 @@
-<?
+<?php
 
 return [
-    'hupun'                => [
-        'api_key'          => env('HUPUN_API_KEY'),
-        'api_serect'       => env('HUPUN_API_SERECT'),
-        'api_options'      => [
-            'api_url'      => env('HUPUN_API_URL'),
+
+    'hupun' => [
+        'api_key' => env('HUPUN_API_KEY'),
+        'api_serect' => env('HUPUN_API_SERECT'),
+        'api_options' => [
+            'api_url' => env('HUPUN_API_URL'),
             'api_work_dir' => storage_path('app/hupun/data'),
-        ]
+        ],
     ],
+
 ];
